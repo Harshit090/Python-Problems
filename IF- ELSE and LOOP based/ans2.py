5d0e7b5bc6ef4e38f0270c989ec9dde10693b37b
Harshit choudhary <harshitchoudhary090@gmail.com>
2019-06-04 21:57:43 +0530

    Add files via upload

---
+++ IF- ELSE and LOOP based/ans2.py
@@ -0,0 +1,8 @@
+for i in range (0, 5):
+    for j in  range (0, i+1):
+        print("*", end=" ")
+    print("\n")
+for i in range (5, 0, -1):
+    for j in  range (i-1, 0, -1):
+        print("*", end=" ")
+    print("\n")
