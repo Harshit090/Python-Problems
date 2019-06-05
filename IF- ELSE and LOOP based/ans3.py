eb518e1796b5fce0d15f5aecdc97dfe9eeafb8d5
Harshit choudhary <harshitchoudhary090@gmail.com>
2019-06-05 08:53:36 +0530

    Add files via upload

---
+++ IF- ELSE and LOOP based/ans3.py
@@ -0,0 +1,6 @@
+a = input("Enter a word")
+new = ''
+
+for i in a:
+    new = i + new
+print(new)
---
+++ IF- ELSE and LOOP based/ans4.py
@@ -0,0 +1,7 @@
+for i in  range (0, 7):
+    if i is 3:
+        continue
+    elif i is 6:
+        continue
+    else:
+        print(i, end=' ')
